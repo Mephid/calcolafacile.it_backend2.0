36793180948b900a0bb1fa0800ed4f2bdf0180e9
G. Cappai <cappaigianluca@gmail.com>
2022-05-21 15:43:25 +0200

    Add Percentage module to calculators main module

--- src/calculators/calculators.module.ts
+++ src/calculators/calculators.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { RouterModule } from '@nestjs/core';
 import { CookedPastaWeightConverterModule } from './cooked-pasta-weight-converter/cooked-pasta-weight-converter.module';
 import { CvKwConverterModule } from './cv-kw-converter/cv-kw-converter.module';
+import { PercentageModule } from './percentage/percentage.module';
 
 /* Applies "calculators" prefix to every route contained in modules*/
 
-const modules = [CookedPastaWeightConverterModule, CvKwConverterModule];
+const modules = [
+  CookedPastaWeightConverterModule,
+  CvKwConverterModule,
+  PercentageModule,
+];
 
 const routerModulesPrefixed = RouterModule.register(
   modules.map((module) => ({ path: 'calculators', module })),
