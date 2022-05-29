5d367754ae4b6f100f595cd83a0c608ace0357d7
G. Cappai <cappaigianluca@gmail.com>
2022-05-29 23:56:04 +0200

    Add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
+import { ConfigModule } from '@nestjs/config';
+
 import { Module } from '@nestjs/common';
 import { CalculatorsModule } from './calculators/calculators.module';
 import { CvKwConverterModule } from './calculators/cv-kw-converter/cv-kw-converter.module';
+import { EmailSenderModule } from './email-sender/email-sender.module';
 
 @Module({
-  imports: [CalculatorsModule, CvKwConverterModule],
+  imports: [
+    CalculatorsModule,
+    CvKwConverterModule,
+    EmailSenderModule,
+    ConfigModule.forRoot(),
+  ],
 })
 export class AppModule {}
--- src/email-sender/email-sender.module.ts
+++ src/email-sender/email-sender.module.ts
@@ -2,29 +2,45 @@ import { Module } from '@nestjs/common';
 import { EmailSenderController } from './email-sender.controller';
 import { EmailSenderService } from './email-sender.service';
 import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [EmailSenderController],
   providers: [EmailSenderService],
   imports: [
-    MailerModule.forRoot({
-      transport: {
-        host: 'smtps.aruba.it',
-        port: 465,
-        // logger: isDev,
-        // debug: isDev,
-        secure: true,
-        auth: {
-          user: process.env.SMTP_USER,
-          pass: process.env.SMTP_PSW,
-        },
-        tls: {
-          minVersion: 'TLSv1.2',
-          // rejectUnauthorized: !isDev,
-        },
-      },
-      defaults: {
-        to: process.env.CONTACT_EMAIL,
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => {
+        const isDev =
+          process.env.NODE_ENV == 'development' ||
+          process.env.NODE_ENV == 'test';
+
+        console.log({
+          user: config.get('SMTP_USER'),
+          pass: config.get('SMTP_PSW'),
+        });
+
+        return {
+          transport: {
+            host: 'smtps.aruba.it',
+            port: 465,
+            logger: isDev,
+            debug: isDev,
+            secure: true,
+            auth: {
+              user: config.get('SMTP_USER'),
+              pass: config.get('SMTP_PSW'),
+            },
+            tls: {
+              minVersion: 'TLSv1.2',
+              rejectUnauthorized: !isDev,
+            },
+          },
+          defaults: {
+            to: config.get('CONTACT_EMAIL'),
+          },
+        };
       },
     }),
   ],
